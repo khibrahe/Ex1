f925292ce1668ef37cbe0d6c92f8ecb163b33d7f
khibrahe <khibrahe@sheridancollege.ca>
2026-09-24 16:20:33 -0400

    added a comment "im done" at the top of the printInfo method

--- ICE1W26-master/src/exercise1/CardTrick.java
+++ ICE1W26-master/src/exercise1/CardTrick.java
@@ -77,6 +77,8 @@ public class CardTrick {
      * @author Paul Bonenfant Jan 2022
      */
     private static void printInfo() {
+        
+//        im done
 
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
